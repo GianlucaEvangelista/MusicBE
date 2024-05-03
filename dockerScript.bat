bb8185f0f7b2ca60733c6d2f73d1063e3908f601
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2024-05-03 12:48:47 +0200

    Delete dockerScript.bat file

--- dockerScript.bat
+++
@@ -1,8 +0,0 @@
-REM Crea l'immagine Docker
-docker build -t music_db_image .
-
-REM Crea un container Docker con il database
-docker run -d --name music_db -p 5432:5432 music_db_image
-
-REM Controlla lo stato del container
-docker ps
