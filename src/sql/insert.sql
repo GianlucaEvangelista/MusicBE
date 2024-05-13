8e5d66f093c0ae98e506c49a3fa6839a848f0071
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2024-05-13 10:06:04 +0200

    Add songs to the insert.sql file

--- src/sql/insert.sql
+++ src/sql/insert.sql
@@ -2,4 +2,6 @@ INSERT INTO songs (title, artist, genre, album, year)
 VALUES
     ('Imagine', 'John Lennon', 'Pop', 'Imagine', '1971'),
     ('Bohemian Rhapsody', 'Queen', 'Rock', 'A Night at the Opera', '1975'),
-    ('Thriller', 'Michael Jackson', 'Pop', 'Thriller', '1982');
+    ('Thriller', 'Michael Jackson', 'Pop', 'Thriller', '1982'),
+    ('Chantaje', 'Shakira feat. Maluma', 'Reggaeton', 'El Dorado', '2016'),
+    ('Hello', 'Adele', 'Pop', '25', '2015');
